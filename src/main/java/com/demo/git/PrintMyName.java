c78aade6d0c3d642b8f1c282cdbe7001bd4cd28b
svaja <smitavaja.uk@gmail.com>
2024-07-20 15:29:57 +0100

    Change name and surname in two seprate line

--- src/main/java/com/demo/git/PrintMyName.java
+++ src/main/java/com/demo/git/PrintMyName.java
@@ -3,6 +3,7 @@ package com.demo.git;
 public class PrintMyName {
 
     public static void main(String[] args) {
-        System.out.println("Smita Vaja");
+        System.out.println("Smita");
+        System.out.println("VAJA");
     }
 }
